701e6553abe23a6a711ca0c368a050d96feb7c6a
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-07-14 11:28:06 +0200

    Header design is completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,18 @@
+import Logo from '../../assets/images/jo-logo.svg'
+
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="header-wrapper">
+            <div className="logo-wrapper">
+                <img src={Logo} alt="Logo" />
+            </div>
+            <div className="nav-wrapper">
+                <nav>
+                    <button className="selected">.projects</button>
+                    <button>.about</button>
+                    <button>.contact</button>
+                </nav>
+            </div>
         </div>
     )
 }
