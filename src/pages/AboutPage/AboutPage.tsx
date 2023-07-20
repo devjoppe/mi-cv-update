d0b64d7c75fe9bd5919ffd0312ea75f23ac74c31
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-07-20 14:22:23 +0200

    Added the about page files and json data

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,8 +1,25 @@
+// Page data
+import about from '../../data/aboutpage.json'
+
 const AboutPage = () => {
     return(
-        <div>
-            ABOUT
-        </div>
+        <>
+            <section className="light-shade top-section">
+                <div className="section-content-wrapper">
+                    {about && about.filter(item => item.id === 1).map(data  => (
+                        <div key={data.id}>
+                            <h1>{data.title}</h1>
+                            <span className="subtitle">
+                                {data.kicker}
+                            </span>
+                            <span className="quote-by">
+                                - {data.user}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
     )
 }
 
