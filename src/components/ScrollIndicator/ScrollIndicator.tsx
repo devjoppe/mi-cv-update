7a07b0f10da142ef1566bfba4d35cbbdc50e2213
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-07-20 14:04:40 +0200

    Added scroll indicator to the header

---
+++ src/components/ScrollIndicator/ScrollIndicator.tsx
@@ -0,0 +1,25 @@
+import {useState} from "react";
+
+const ScrollIndicator = () => {
+
+    const [scroll, setScroll] = useState<number>(0)
+
+    const onScroll = () => {
+        const scrolled = document.documentElement.scrollTop
+        const maxHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        const scrollPercent = (scrolled / maxHeight) * 100
+        setScroll(scrollPercent)
+    }
+
+    window.addEventListener("scroll", onScroll)
+
+    console.log(scroll)
+
+    return(
+        <div className="scroll-indicator">
+            <div className="bar" style={{width: `${scroll}%`}}></div>
+        </div>
+    )
+}
+
+export default ScrollIndicator
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,11 +4,13 @@ import {Outlet} from "react-router-dom";
 // Components
 import Header from "../components/Header/Header.tsx";
 import Footer from "../components/Footer/Footer.tsx";
+import ScrollIndicator from "../components/ScrollIndicator/ScrollIndicator.tsx";
 
 const Layout = () => {
     return(
         <div className="website-container">
             <Header />
+            <ScrollIndicator />
             <Outlet />
             <Footer />
         </div>
