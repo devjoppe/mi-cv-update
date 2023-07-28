fb5caf230f656c42bd9e250e35528572d6ccd447
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-07-28 12:15:43 +0200

    Changing header background based on what page the user is on

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,23 +3,30 @@ import Logo from '../../assets/images/jo-logo.svg'
 // React Router
 import {NavLink} from "react-router-dom";
 
+// React
+import {useState} from "react";
+
 const Header = () => {
+
+    const [isLight, setIsLight] = useState<boolean>(false)
+
+    console.log("isLight: ", isLight)
 
     return (
         <header>
-            <div className="header-wrapper">
+            <div className={`${isLight ? "light-shade" : "dark-shade"} header-wrapper`} >
                 <div className="logo-wrapper">
                     <img src={Logo} alt="Logo" />
                 </div>
                 <div className="nav-wrapper">
                     <nav>
-                        <NavLink to="/" className={({ isActive }) => isActive ? "active" : "standard" } >
+                        <NavLink to="/" className={({ isActive }) => isActive ? "active" : "standard" } onClick={() => setIsLight(false)}>
                             projects
                         </NavLink>
-                        <NavLink to="/about" className={({ isActive }) => isActive ? "active" : "standard" } >
+                        <NavLink to="/about" className={({ isActive }) => isActive ? "active" : "standard" } onClick={() => setIsLight(true)}>
                             about
                         </NavLink>
-                        <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : "standard" } >
+                        <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : "standard" } onClick={() => setIsLight(false)}>
                             contact
                         </NavLink>
                     </nav>
