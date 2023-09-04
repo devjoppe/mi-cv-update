b4bb0ad8af03797596951ccb6ac2580021f5e7ba
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-09-04 08:56:35 +0200

    Fixed links and re-styled li-tags.

--- src/components/LinkList/LinkList.tsx
+++ src/components/LinkList/LinkList.tsx
@@ -3,6 +3,9 @@ import React from "react"
 // Interface
 import {linksInt} from "../../interfaces/linksInt.ts";
 
+// Icons
+import {FaExternalLinkSquareAlt} from "react-icons/fa";
+
 interface IProp {
     linkData: linksInt,
     style: number
@@ -13,7 +16,7 @@ const LinkList:React.FC<IProp> = ({linkData, style}) => {
     return(
         <>
             {style === 0 &&
-                <li><a href={linkData.url} target="_blank">{linkData.link_title}</a> - {linkData.description}</li>}
+                <li><FaExternalLinkSquareAlt size={13} /> <a href={linkData.url} target="_blank">{linkData.link_title}</a> - {linkData.description}</li>}
             {style === 1 &&
                 <li><a href={linkData.url} target="_blank">{linkData.link_title}</a><br />
                     {linkData.description}
