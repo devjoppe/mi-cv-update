8992116003e11285f4bc37099edb4c4c7d261dc6
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2024-01-22 16:25:34 +0100

    commented out hello and welcome...

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -10,14 +10,20 @@ import UseSection from "./UseSection.tsx";
 // Redux - Change light - useDispatch for changing value, useSelector for accessing.
 import {useDispatch} from "react-redux";
 import {changeLight} from "../../features/isLight.ts"; // Redux feature to change light
+// import {isModeRootStateInt} from "../../features/isMode.ts";
+
+// Background images for the top section
+// import darkBg from '../../assets/images/jotto-background-smoke-dark.webp'
+// import lightBg from '../../assets/images/jotto-background-smoke-light.webp'
 
 // React
 import {useEffect} from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
 const StartPage = () => {
 
     const dispatch = useDispatch()
+    // const themeState = useSelector((state:isModeRootStateInt) => state.isMode.mode)
 
     useEffect(() => {
         dispatch(changeLight(false))
@@ -25,11 +31,11 @@ const StartPage = () => {
 
     return(
         <>
-            <section className="dark-shade top-section">
+            <section className="dark-shade top-section" >
                 <div className="section-content-wrapper" data-aos="zoom-out" data-aos-duration="1500">
                     {start && start.filter(item => item.id === 1).map(data  => (
                         <div key={data.id}>
-                            <span className="welcome-text">Hello! 👋 My name is <Link to={'/about'}>Joakim</Link> and I am a</span>
+                            { /* <span className="welcome-text">Hello! 👋 My name is <Link to={'/about'}>Joakim</Link> and I am a</span> */}
                             <h1>{data.title}</h1>
                             <span className="subtitle">
                                 {data.kicker}
