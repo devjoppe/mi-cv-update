0ac7582df96dfb49a0a4c3c2c7e280d1de32c4ba
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2024-03-04 17:50:49 +0100

    this is a special section that I dont know what it is for? But commiting it

---
+++ src/pages/StartPage/SpecialSection.tsx
@@ -0,0 +1,20 @@
+const SpecialSection = () => {
+    return(
+        <section className="inverted-shade">
+            <div className="section-content-wrapper">
+                <div>
+                    <div>
+                        <h2>What I do</h2>
+                    </div>
+                    <div>
+                        <span className="inverted-kicker">
+                            Kicker text
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SpecialSection
